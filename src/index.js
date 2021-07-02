f8287ddd3d96eb853f9c610706f3a47baf74fccd
Jeremy Myers <inernestmusic@gmail.com>
2021-07-02 16:46:21 -0500

    added a toggle slider and adjusted some styling. in the middle of repainting the DOM when units are toggled. need to add a condition to the forecast function that displays the temperature units based on the units passed into oneCall. also need to toggle the class displays in the currentWeather container when units are toggled.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@ import { callCurrentWeatherAPI } from "./handleAPI.js";
 
 const searchInput = document.querySelector(`#input-location`);
 const submitButton = document.querySelector(`#submit-location`);
+const toggleUnits = document.querySelector(`#toggle-units`);
 // const weatherIcon = document.querySelector(`img`);
 
 submitButton.addEventListener(`click`, (e) => {
   e.preventDefault();
   callCurrentWeatherAPI(searchInput.value, `imperial`);
   searchInput.value = ``;
+});
+
+toggleUnits.addEventListener(`change`, (e) => {
+  console.log(e.target.checked);
+  console.log(
+    e.target.parentElement.parentElement.nextElementSibling.firstElementChild
+      .firstElementChild.textContent
+  );
+
+  const cityOnDisplay =
+    e.target.parentElement.parentElement.nextElementSibling.firstElementChild
+      .firstElementChild.textContent;
+  let unitsToDisplay = `imperial`;
+  if (e.target.checked) {
+    unitsToDisplay = `metric`;
+  }
+  callCurrentWeatherAPI(cityOnDisplay, unitsToDisplay);
 });
 
 // default weather on page load
